d6a39b715dc9d998e61343e08417991b1fc9051f
Chernikov Nikita <14nik20@gmail.com>
2022-03-29 19:19:43 +0300

    Changed gitignore

---
+++ Business/Other/MailRequest.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Business.Other
+{
+    public class MailRequest
+    {
+        public string To { get; set; } = string.Empty;
+        public string Subject { get; set; } = string.Empty;
+        public string Body { get; set; } = string.Empty;
+        public List<IFormFile> Attachments { get; set; } = new List<IFormFile>();
+    }
+}
