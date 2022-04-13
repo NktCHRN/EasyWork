b0c58273fe8a8804409e47d7647050974d1c3da9
Chernikov Nikita <14nik20@gmail.com>
2022-04-14 00:12:46 +0300

    Fixed bugs

--- WebAPI/DTOs/ResendEmailConfirmationDTO.cs
+++ WebAPI/DTOs/ResendEmailConfirmationDTO.cs
@@ -6,7 +6,7 @@ namespace WebAPI.DTOs
     {
         [Required(AllowEmptyStrings = false)]
         [EmailAddress]
-        public string Email { get; set; } = string.Empty;
+        public string Email { get; init; } = string.Empty;
 
         [Required(AllowEmptyStrings = false)]
         public string ClientURI { get; init; } = string.Empty;
